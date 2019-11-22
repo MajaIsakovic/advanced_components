9fd14caaff8bf6cb0a10a3a180ea4c7a12dc0d34
Maja Isakovic <majamaja733@hotmail.com>
2019-11-22 11:55:36 +0100

    first commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>This is a quote</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    p{
+        border: 2px solid #ccc;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+        border-radius: 3px;
+        color: rgb(37, 37, 37);
+    }
+</style>
